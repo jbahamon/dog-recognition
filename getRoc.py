2b15435b80125f54e680838948d08295f628b88f
segonzal <seba-5@hotmail.com>
2014-10-25 22:38:17 -0300

    Extraidos los puntos de ROC

---
+++ getRoc.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+#1 : dog
+#0 : no dog
+def processFile(inFile,thethas):
+    #header doesn't help us
+    inFile.readline()
+    # now read what is important
+    current_positives = 0.0
+    TP = [0.0] * len(thethas)
+    FP = [0.0] * len(thethas)
+    
+    j = 0
+    for line in inFile:
+        j += 1
+        line = line.split();
+        # asigned class, class1 prob, class0 prob
+        line = [int(line[0]),float(line[1]),float(line[2])]
+        
+        current_positives += line[0]
+        for i in range(len(thethas)):
+            #dog_P / nodog_P
+            if line[1] / line[2] > thethas[i]:
+                if j < 200:#is dog?
+                    TP[i] += 1
+                else:
+                    FP[i] += 1
+    
+    total_positives = current_positives
+    total_negatives = 400 - total_positives
+    
+    TPR = [ positives / total_positives for positives in TP ]
+    FPR = [ positives / total_negatives for positives in FP ]
+    
+    return zip(FPR,TPR)
+
+files = [
+        "results100",
+        "results500",
+        "results1000",
+        "results1500"
+        ]
+
+out = open("rocPoints.csv","w")
+for f in files:
+    inF = open(f,"r")
+    arr = [f] + processFile(inF,np.arange(0.1, 0.9, 0.05))
+    out.write(str(arr)+'\n')
+    inF.close()
+out.close()
